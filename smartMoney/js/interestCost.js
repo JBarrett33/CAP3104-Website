1e3c5212039c90e24584279f1f0d8f1ffca9256d
JBarrett33 <97552924+JBarrett33@users.noreply.github.com>
2023-02-22 11:32:40 -0500

    Bugfixes
    * Fixed negative number issues in the cost of interest calculator (Thanks Antonio!)

--- smartMoney/js/interestCost.js
+++ smartMoney/js/interestCost.js
@@ -1,12 +1,25 @@
 function validate(){
-    return !(document.getElementById("interest-salestax").value == "" || 
+    let isPopulated = !(document.getElementById("interest-salestax").value == "" || 
         document.getElementById("interest-interestrate").value == "" || 
         document.getElementById("interest-itemcost").value == "" || 
         document.getElementById("interest-monthlypayment").value == "")
+
+    let isValidRange = document.getElementById("interest-salestax").value >= 0 &&
+        document.getElementById("interest-interestrate").value >= 0 &&
+        document.getElementById("interest-itemcost").value > 0 &&
+        document.getElementById("interest-monthlypayment").value > 0
+
+    return isPopulated && isValidRange
 }
 
 function updateInterest(){
     if(!validate()){
+        document.getElementById("interest-table-itemprice").innerHTML = "";
+        document.getElementById("interest-table-interest").innerHTML = "";
+        document.getElementById("interest-table-salestax").innerHTML = "";
+        document.getElementById("interest-table-months").innerHTML = "";
+        document.getElementById("interest-table-total").innerHTML = "";
+        window.alert("Input error! Please check your input values.")
         return
     }
     taxRate = document.getElementById("interest-salestax").value / 100;
